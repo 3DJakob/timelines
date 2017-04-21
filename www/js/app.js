291305d27ddc422f27f9da95bf770f4e19eac8d4
Jakob Unnebäck <jakob@folkdatorn.se>
2017-04-21 14:30:15 +0200

    Feature: Editmode
    Editmode for future feature of deleting projects.

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,5 @@
 var projects = []
+var edit = false
 
 function newProject () {
   var projectName = prompt('Project name', '')
@@ -10,12 +11,22 @@ function newProject () {
   }
 }
 
+function editToggle () {
+  edit = !edit
+  var elements = document.getElementsByClassName('deleteBtn')
+  if (edit === true) {
+    document.getElementsByTagName('body')[0].className = 'editmode'
+  } else {
+    document.getElementsByTagName('body')[0].className = 'standard'
+  }
+}
+
 function expand (project) {
   var element = document.getElementById(project)
   var style = window.getComputedStyle(element)
   var height = style.getPropertyValue('height')
   if (height === '0px') {
-    element.style.maxHeight = '200px'
+    element.style.maxHeight = '100vh'
   } else {
     element.style.maxHeight = '0'
   }
@@ -27,7 +38,9 @@ function render () {
   if (projects.lenght !== 0) {
     projects.forEach(function (project) {
       var div = document.createElement('div')
+      var row = document.createElement('div')
       var h2 = document.createElement('h2')
+      var remove = document.createElement('button')
       var content = document.createElement('div')
       var input = document.createElement('input')
       var button = document.createElement('button')
@@ -38,11 +51,15 @@ function render () {
       div.style.backgroundColor = project.color
       div.classList = 'project'
       h2.textContent = project.name + ' ▿'
+      row.classList = 'row'
+      remove.textContent = 'Delete'
       button.textContent = 'New activity'
       content.setAttribute('id', project.name)
       content.style.maxHeight = '0'
 
-      div.appendChild(h2)
+      row.appendChild(h2)
+      row.appendChild(remove)
+      div.appendChild(row)
       content.appendChild(input)
       content.appendChild(button)
       div.appendChild(content)
